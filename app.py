deb228b48935ab3034fedcae38dc248afd0faec8
Saqib Sharief <71658974+Saqibahamed@users.noreply.github.com>
2026-04-11 15:37:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("model/churn_model.pkl")
+
+st.title("Customer Churn Prediction")
+
+# Inputs
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", ["Yes", "No"])
+Partner = st.selectbox("Partner", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["Yes", "No"])
+tenure = st.slider("Tenure", 0, 1000, 12)
+PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
+MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No"])
+InternetService = st.selectbox("Internet Service", ["DSL", "Fiber optic", "No"])
+OnlineSecurity = st.selectbox("Online Security", ["Yes", "No"])
+OnlineBackup = st.selectbox("Online Backup", ["Yes", "No"])
+DeviceProtection = st.selectbox("Device Protection", ["Yes", "No"])
+TechSupport = st.selectbox("Tech Support", ["Yes", "No"])
+StreamingTV = st.selectbox("Streaming TV", ["Yes", "No"])
+StreamingMovies = st.selectbox("Streaming Movies", ["Yes", "No"])
+Contract = st.selectbox("Contract", ["Month-to-month", "One year", "Two year"])
+PaperlessBilling = st.selectbox("Paperless Billing", ["Yes", "No"])
+PaymentMethod = st.selectbox("Payment Method", [
+    "Electronic check", "Mailed check", "Bank transfer (automatic)", "Credit card (automatic)"
+])
+MonthlyCharges = st.number_input("Monthly Charges", 0, 200, 50)
+
+# Create dataframe
+input_data = pd.DataFrame({
+    'gender': [gender],
+    'SeniorCitizen': [SeniorCitizen],
+    'Partner': [Partner],
+    'Dependents': [Dependents],
+    'tenure': [tenure],
+    'PhoneService': [PhoneService],
+    'MultipleLines': [MultipleLines],
+    'InternetService': [InternetService],
+    'OnlineSecurity': [OnlineSecurity],
+    'OnlineBackup': [OnlineBackup],
+    'DeviceProtection': [DeviceProtection],
+    'TechSupport': [TechSupport],
+    'StreamingTV': [StreamingTV],
+    'StreamingMovies': [StreamingMovies],
+    'Contract': [Contract],
+    'PaperlessBilling': [PaperlessBilling],
+    'PaymentMethod': [PaymentMethod],
+    'MonthlyCharges': [MonthlyCharges]
+})
+
+# Predict
+if st.button("Predict"):
+    prob = model.predict_proba(input_data)[:, 1][0]
+    prediction = 1 if prob > 0.3 else 0
+
+    st.subheader(f"Churn Probability: {prob:.2f}")
+
+    if prediction == 1:
+        st.error("⚠️ Customer is likely to churn")
+    else:
+        st.success("✅ Customer is likely to stay")
